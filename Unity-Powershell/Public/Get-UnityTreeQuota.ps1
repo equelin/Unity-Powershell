b12b441aabfdf647903098fc3f39c5628268a2c8
A. Hugas <ahugas@gmail.com>
2019-05-22 23:53:26 +0200

    feature: Add TreeQuota Management

---
+++ Unity-Powershell/Public/Get-UnityTreeQuota.ps1
@@ -0,0 +1,56 @@
+Function Get-UnityTreeQuota {
+
+  <#
+      .SYNOPSIS
+      Queries the EMC Unity array to retrieve informations about treequotas.
+      .DESCRIPTION
+      Querries the EMC Unity array to retrieve informations about treequotas.
+      You need to have an active session with the array.
+      .NOTES
+      Written by Albert Hugas under MIT licence - https://github.com/equelin/Unity-Powershell/blob/master/LICENSE
+      .LINK
+      https://github.com/equelin/Unity-Powershell
+      .PARAMETER Session
+      Specifies an UnitySession Object.
+      .PARAMETER Path
+      Specifies the treeQuota Path.
+      .PARAMETER ID
+      Specifies the object ID.
+      .EXAMPLE
+      Get-UnityTreeQuota
+
+      Retrieve information about treequota
+      .EXAMPLE
+      Get-UnityTreeQuota -Id 'treequota_123456_12'
+
+      Retrieves information about treequota id treequota_123456_12
+  #>
+
+  [CmdletBinding(DefaultParameterSetName="ID")]
+  Param (
+    [Parameter(Mandatory = $false,HelpMessage = 'EMC Unity Session')]
+    $session = ($global:DefaultUnitySession | where-object {$_.IsConnected -eq $true}),
+    [Parameter(Mandatory = $false,ParameterSetName="Path",ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True,HelpMessage = 'Filesystem Name')]
+    [String[]]$Path,
+    [Parameter(Mandatory = $false,ParameterSetName="ID",ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True,HelpMessage = 'Filesystem ID')]
+    [String[]]$ID
+  )
+
+  Begin {
+    Write-Debug -Message "[$($MyInvocation.MyCommand)] Executing function"
+
+    #Initialazing variables
+    $URI = '/api/types/treeQuota/instances' #URI
+    $Typename = "UnitytreeQuota"
+  }
+
+  Process {
+    Foreach ($sess in $session) {
+
+      Write-Debug -Message "[$($MyInvocation.MyCommand)] Processing Session: $($Session.Server) with SessionId: $($Session.SessionId)"
+
+      Get-UnityItemByKey -Session $Sess -URI $URI -Typename $Typename -Key $PsCmdlet.ParameterSetName -Value $PSBoundParameters[$PsCmdlet.ParameterSetName]
+
+    } # End Foreach ($sess in $session)
+  } # End Process
+} # End Function
